b32f94f4e5d6ec2f4ef56a9c8f3520c48661c490
name <kydea2240@naver.com>
2024-05-21 16:30:46 +0900

    vistied 관련
    api 3개에 preiod와 visitor 형태로 일단 구현

--- DreamBulider/src/main/java/codehows/dream/dreambulider/entity/Visited.java
+++ DreamBulider/src/main/java/codehows/dream/dreambulider/entity/Visited.java
@@ -5,8 +5,16 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.*;
+
+import java.time.LocalDate;
 
 @Entity
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Visited {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -15,5 +23,8 @@ public class Visited {
 
 	@Column(nullable = false)
 	private String visitedIp;
+
+	@Column(nullable = false)
+	private LocalDate visitDate;
 
 }
