7b5f0a317ecb5081bde5eaefc846b1bbe0fe869f
song <usnara9465@naver.com>
2024-05-22 17:49:55 +0900

    board
    5.22 관리자 완성, 마이페이지 하는중

--- DreamBulider/src/main/java/codehows/dream/dreambulider/constats/Authority.java
+++
@@ -1,5 +0,0 @@
-package codehows.dream.dreambulider.constats;
-
-public enum Authority {
-	ROLE_USER,ROLE_ADMIN
-}
--- DreamBulider/src/main/java/codehows/dream/dreambulider/repository/NestedReplyRepository.java
+++
@@ -1,7 +0,0 @@
-package codehows.dream.dreambulider.repository;
-
-import codehows.dream.dreambulider.entity.NestedReply;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface NestedReplyRepository extends JpaRepository <NestedReply,Long> {
-}
